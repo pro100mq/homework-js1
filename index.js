749aa045961d33304ababcb4c725cec3e90d4ad3
pro100mq <144606932+pro100mq@users.noreply.github.com>
2024-09-27 17:13:51 +0300

    homework-js1

---
+++ index.js
@@ -0,0 +1,36 @@
+const myAge = "17";
+console.log(myAge);
+
+const myName = "kiril";
+console.log(myName);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Неважливо, як повільно ти йдеш, головне - не зупиняйся";
+console.log(myString);
+
+let myNumber = 17;
+let myNumbers = myNumber + 10;
+console.log(myNumbers);
+
+const myNull = null;
+console.log(myNull);
+
+const myNickname = prompt("Введіть ім'я користувача")
+console.log(myNickname)
+
+const myGreeting = "Вітаємо з використанням ім'ям користувача";
+console.log(typeof myGreeting)
+
+alert(myGreeting)
+
+  let response = confirm("Ви впевнені, що хочете підтвердити дію?");
+
+  if (response) {
+    alert("Дякую за підтвердження!");
+  } 
+  
+  else {
+    alert("Дію відмінено!");
+  }
