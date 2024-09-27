1504452e063ea9fe89f601afffe5bdf0cb623e93
pro100mq <144606932+pro100mq@users.noreply.github.com>
2024-09-27 17:42:24 +0300

    update

--- index.js
+++ index.js
@@ -17,20 +17,35 @@ console.log(myNumbers);
 const myNull = null;
 console.log(myNull);
 
-const myNickname = prompt("Введіть ім'я користувача")
-console.log(myNickname)
+const myNickname = prompt("Введіть ім'я користувача");
+console.log(myNickname);
 
 const myGreeting = "Вітаємо з використанням ім'ям користувача";
-console.log(typeof myGreeting)
+console.log(typeof myGreeting);
 
-alert(myGreeting)
+alert(myGreeting);
 
-  let response = confirm("Ви впевнені, що хочете підтвердити дію?");
+const response = confirm("Ви впевнені, що хочете підтвердити дію?");
 
-  if (response) {
-    alert("Дякую за підтвердження!");
-  } 
-  
-  else {
-    alert("Дію відмінено!");
-  }
+if (response) {
+  alert("Дякую за підтвердження!")
+} else {
+  alert("Дію відмінено!")
+};
+
+const myDangerous = "Увага, дія можливо є небезпечною";
+alert(myDangerous);
+
+// let response = confirm("Ви впевнені, що хочете продовжити?")
+// console.log(response)
+
+let myResponse = confirm("Хочете підтвердити дію?");
+console.log(myResponse)
+
+    if (response) {
+        alert("Дякую за підтвердження");
+    } 
+
+    else {
+        alert("Дію відмінено");
+    }
